29b024f1bd7bc26887ad03bf433f66cd8e016203
Luis Molina <luis_molina@outlook.com>
2017-11-13 00:54:33 -0700

    Added validation to DatabaseController.

--- src/sample/DatabaseController.java
+++ src/sample/DatabaseController.java
@@ -1,22 +1,40 @@
 package sample;
 
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RequiredFieldValidator;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
 
 public class DatabaseController {
 
-    @FXML private TextField newEmployeeNameField;
-    @FXML private TextField newEmployeePositionField;
-    @FXML private TextField selectedEmployeeName;
-    @FXML private TextField selectedEmployeePosition;
+    @FXML private JFXTextField newEmployeeNameField;
+    @FXML private JFXTextField newEmployeePositionField;
+    @FXML private JFXTextField selectedEmployeeName;
+    @FXML private JFXTextField selectedEmployeePosition;
+
+
+    @FXML
+    private void initialize() {
+        RequiredFieldValidator nameRequired = new RequiredFieldValidator();
+        RequiredFieldValidator positionRequired = new RequiredFieldValidator();
+        RequiredFieldValidator selectedEmployeeRequired = new RequiredFieldValidator();
+
+        nameRequired.setMessage("Name required");
+        positionRequired.setMessage("Position required");
+        selectedEmployeeRequired.setMessage("Select a row from the table");
+
+        newEmployeeNameField.getValidators().add(nameRequired);
+        newEmployeePositionField.getValidators().add(positionRequired);
+        selectedEmployeePosition.getValidators().add(selectedEmployeeRequired);
+    }
 
     @FXML
     private void onCreate() {
-
+        newEmployeeNameField.validate();
+        newEmployeePositionField.validate();
     }
 
     @FXML
     private void onDecrypt() {
-
+        selectedEmployeePosition.validate();
     }
 }
