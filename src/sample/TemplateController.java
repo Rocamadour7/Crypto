12e4bdc4dd27403180682b62579b1da2e747ca12
Luis Molina <luis_molina@outlook.com>
2017-11-13 03:46:43 -0700

    Added back button.

--- src/sample/TemplateController.java
+++ src/sample/TemplateController.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -43,5 +44,16 @@ public class TemplateController {
     private void minimizeWindow() {
         Stage stage = (Stage) minimizeButton.getScene().getWindow();
         stage.setIconified(true);
+    }
+
+    @FXML
+    public void backToHome() {
+        try {
+            root = FXMLLoader.load(getClass().getResource("template.fxml"));
+            Stage primaryStage = (Stage) minimizeButton.getScene().getWindow();
+            primaryStage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
